c01f049c776b2360411e8c95ff230f6412abfba2
yang <yang@fydeos.io>
2021-01-08 13:42:29 +0800

    fix hdmi audio out for rpi400

--- overlay-rpi4-fydeos/chromeos-base/chromeos-bsp-rpi4-fydeos/files/audio/set-hdmi.sh
+++ overlay-rpi4-fydeos/chromeos-base/chromeos-bsp-rpi4-fydeos/files/audio/set-hdmi.sh
@@ -3,4 +3,5 @@ cras_client=/usr/bin/cras_test_client
 hdmi_fix_id=e9849cd9
 dev=$(${cras_client} --dump_s |grep $hdmi_fix_id | awk '{print $2}')
 ${cras_client} --plug "${dev}":1
-logger -t "rpi4-hdmi" "set ${dev} plugged"
+${cras_client} --select_output ${dev}
+logger -t "rpi4-hdmi" "set ${dev} plugged and to be the output dev."
